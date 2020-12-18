75e9f49526942a84f34a7eca3df4d53f88d4f6a4
min3rv4 <59661336+min3rv4@users.noreply.github.com>
2020-12-18 15:39:42 -0500

    Create ExportAD.ps1

---
+++ ExportAD.ps1
@@ -0,0 +1,8 @@
+Get-ADUser -filter * -properties passwordlastset | select name, passwordlastset | Export-csv "passwordsLastSet.csv"
+
+Select-Object GivenName, Surname, Title, SamAccountName, Manager, DisplayName, City, EmailAddress, EmployeeID, Enabled, Department, OfficePhone, MobilePhone, LockedOut, LockOutTime, AccountExpirationDate, PasswordExpired, PasswordLastSet, LastLogonDate
+
+
+Get-ADUser -filter * |Select GivenName, Surname, Title, SamAccountName, Manager, DisplayName, City, EmailAddress, EmployeeID, Enabled, Department, OfficePhone, MobilePhone, LockedOut, LockOutTime, AccountExpirationDate, PasswordExpired, PasswordLastSet, LastLogonDate
+
+| Export-csv "passwordsLastSetFull.csv"
