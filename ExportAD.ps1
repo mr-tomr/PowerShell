d31503d8204f44fb5a398388aa08cb93cfad3746
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-12-19 11:24:07 -0500

    Situational Awareness - LOL BIN

--- ExportAD.ps1
+++ ExportAD.ps1
@@ -5,4 +5,4 @@ Select-Object GivenName, Surname, Title, SamAccountName, Manager, DisplayName, C
 
 Get-ADUser -filter * |Select GivenName, Surname, Title, SamAccountName, Manager, DisplayName, City, EmailAddress, EmployeeID, Enabled, Department, OfficePhone, MobilePhone, LockedOut, LockOutTime, AccountExpirationDate, PasswordExpired, PasswordLastSet, LastLogonDate
 
-| Export-csv "passwordsLastSetFull.csv"
+| Export-csv "passwordsLastSetFull.csv" 
