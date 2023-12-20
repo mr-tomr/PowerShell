0958f82c2e9b718f43c2d7dcd339046abc84d5af
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-12-20 13:17:17 -0500

    OSCP

--- ServicesNotInSystem-List.ps1
+++ ServicesNotInSystem-List.ps1
@@ -2,4 +2,8 @@
 
 # Use to locate services whose paths are not the System Directory.
 
+# Running Serivces
 Get-CimInstance -ClassName win32_service | Select Name, State, PathName | Where-Object { $_.State -like 'Running' -and $_.PathName -notlike "C:\Windows\system32\*" }
+
+# Running or Stopped - Better View
+Get-CimInstance -ClassName win32_service | Select Name, State, PathName | Where-Object { $_.PathName -notlike "C:\Windows\system32\*" }
