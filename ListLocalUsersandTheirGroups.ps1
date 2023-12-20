93f2c111fc9f9164a17c43c3645391343236b906
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-12-20 10:26:05 -0500

    OSCP

--- ListLocalUsersandTheirGroups.ps1
+++ ListLocalUsersandTheirGroups.ps1
@@ -3,3 +3,4 @@
 # One-liner to list all local users on computer and display their group memberships.
 
 Get-WmiObject -Class Win32_UserAccount | ForEach-Object { $user = $_; $groups = $user.GetRelated('Win32_Group') | ForEach-Object { $_.Name }; Write-Host "User: $($user.Name)`nGroups: $($groups -join ', ')`n" }
+
